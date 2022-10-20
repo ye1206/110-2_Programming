25ccf2cf56da5e7fb3a054764e976bf023e7eb85
ye1206 <116014300+ye1206@users.noreply.github.com>
2022-10-20 21:51:40 +0800

    Lottery Number

---
+++ Lottery Number/LotteryNumber_Cheng3001/FrmLotteryNumber_Cheng3001.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace LotteryNumber_Cheng3001
+{
+    partial class FrmLotteryNumber_Cheng3001
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl1st = new System.Windows.Forms.Label();
+            this.Lbl8th = new System.Windows.Forms.Label();
+            this.Lbl7th = new System.Windows.Forms.Label();
+            this.Lbl6th = new System.Windows.Forms.Label();
+            this.Lbl10th = new System.Windows.Forms.Label();
+            this.Lbl9th = new System.Windows.Forms.Label();
+            this.Lbl5th = new System.Windows.Forms.Label();
+            this.Lbl4th = new System.Windows.Forms.Label();
+            this.Lbl3rd = new System.Windows.Forms.Label();
+            this.Lbl2nd = new System.Windows.Forms.Label();
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Lbl1st
+            // 
+            this.Lbl1st.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl1st.Location = new System.Drawing.Point(21, 12);
+            this.Lbl1st.Name = "Lbl1st";
+            this.Lbl1st.Size = new System.Drawing.Size(63, 32);
+            this.Lbl1st.TabIndex = 0;
+            this.Lbl1st.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl8th
+            // 
+            this.Lbl8th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl8th.Location = new System.Drawing.Point(607, 12);
+            this.Lbl8th.Name = "Lbl8th";
+            this.Lbl8th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl8th.TabIndex = 1;
+            this.Lbl8th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl7th
+            // 
+            this.Lbl7th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl7th.Location = new System.Drawing.Point(535, 12);
+            this.Lbl7th.Name = "Lbl7th";
+            this.Lbl7th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl7th.TabIndex = 2;
+            this.Lbl7th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl6th
+            // 
+            this.Lbl6th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl6th.Location = new System.Drawing.Point(463, 12);
+            this.Lbl6th.Name = "Lbl6th";
+            this.Lbl6th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl6th.TabIndex = 3;
+            this.Lbl6th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl10th
+            // 
+            this.Lbl10th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl10th.Location = new System.Drawing.Point(751, 12);
+            this.Lbl10th.Name = "Lbl10th";
+            this.Lbl10th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl10th.TabIndex = 4;
+            this.Lbl10th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl9th
+            // 
+            this.Lbl9th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl9th.Location = new System.Drawing.Point(679, 12);
+            this.Lbl9th.Name = "Lbl9th";
+            this.Lbl9th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl9th.TabIndex = 5;
+            this.Lbl9th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl5th
+            // 
+            this.Lbl5th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl5th.Location = new System.Drawing.Point(309, 12);
+            this.Lbl5th.Name = "Lbl5th";
+            this.Lbl5th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl5th.TabIndex = 6;
+            this.Lbl5th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl4th
+            // 
+            this.Lbl4th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl4th.Location = new System.Drawing.Point(237, 12);
+            this.Lbl4th.Name = "Lbl4th";
+            this.Lbl4th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl4th.TabIndex = 7;
+            this.Lbl4th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl3rd
+            // 
+            this.Lbl3rd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl3rd.Location = new System.Drawing.Point(165, 12);
+            this.Lbl3rd.Name = "Lbl3rd";
+            this.Lbl3rd.Size = new System.Drawing.Size(63, 32);
+            this.Lbl3rd.TabIndex = 8;
+            this.Lbl3rd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl2nd
+            // 
+            this.Lbl2nd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl2nd.Location = new System.Drawing.Point(93, 12);
+            this.Lbl2nd.Name = "Lbl2nd";
+            this.Lbl2nd.Size = new System.Drawing.Size(63, 32);
+            this.Lbl2nd.TabIndex = 9;
+            this.Lbl2nd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(309, 90);
+            this.BtnGenerate.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(112, 60);
+            this.BtnGenerate.TabIndex = 10;
+            this.BtnGenerate.Text = "Generate Numbers";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(430, 90);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(112, 60);
+            this.BtnExit.TabIndex = 11;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // FrmLotteryNumber_Cheng3001
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 163);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnGenerate);
+            this.Controls.Add(this.Lbl2nd);
+            this.Controls.Add(this.Lbl3rd);
+            this.Controls.Add(this.Lbl4th);
+            this.Controls.Add(this.Lbl5th);
+            this.Controls.Add(this.Lbl9th);
+            this.Controls.Add(this.Lbl10th);
+            this.Controls.Add(this.Lbl6th);
+            this.Controls.Add(this.Lbl7th);
+            this.Controls.Add(this.Lbl8th);
+            this.Controls.Add(this.Lbl1st);
+            this.Name = "FrmLotteryNumber_Cheng3001";
+            this.Text = "Lottery Number";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl1st;
+        private System.Windows.Forms.Label Lbl8th;
+        private System.Windows.Forms.Label Lbl7th;
+        private System.Windows.Forms.Label Lbl6th;
+        private System.Windows.Forms.Label Lbl10th;
+        private System.Windows.Forms.Label Lbl9th;
+        private System.Windows.Forms.Label Lbl5th;
+        private System.Windows.Forms.Label Lbl4th;
+        private System.Windows.Forms.Label Lbl3rd;
+        private System.Windows.Forms.Label Lbl2nd;
+        private System.Windows.Forms.Button BtnGenerate;
+        private System.Windows.Forms.Button BtnExit;
+    }
+}
---
+++ Lottery Number/LotteryNumber_Cheng3001/FrmLotteryNumber_Cheng3001.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryNumber_Cheng3001
+{
+    public partial class FrmLotteryNumber_Cheng3001 : Form
+    {
+        #region global
+        const int SIZE = 10;
+        int[] lotteryNumber = new int[SIZE];
+        Label[] randomLabel = new Label[SIZE];
+        #endregion global
+
+        public FrmLotteryNumber_Cheng3001()
+        {
+            InitializeComponent();
+            initializeLabelArray();
+        }
+
+        #region Button
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            for (int index = 0; index < lotteryNumber.Length; index++)
+            {
+                lotteryNumber[index] = rnd.Next(1, 100);
+                randomLabel[index].Text = lotteryNumber[index].ToString();  //將lotterynumber按照索引指定給相同位置的label
+
+                //Before:    Lbl1st.Text = lotteryNumber[0].ToString();     (Others the same as Lbl1st)
+            }
+
+            var distinctedNumbers = produceNoDuplicatedData(lotteryNumber); //When use "var" to define a variance, you don't need to give a data type. It will define its type automatically by the things you give behind.
+
+            for (int index = 0; index < lotteryNumber.Length; index++)
+            {
+                randomLabel[index].Text = distinctedNumbers[index].ToString();  //Same as Line 35
+            }
+        } //end of BtnGenerate_Click
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        } //end of BtnExit_Click
+        #endregion Button
+
+        #region function
+        private void initializeLabelArray()
+        {
+            Label[] randomLabel2 = { Lbl1st, Lbl2nd, Lbl3rd, Lbl4th, Lbl5th, Lbl6th, Lbl7th, Lbl8th, Lbl9th, Lbl10th };
+            randomLabel = randomLabel2;
+        } //end of initializeLabelArray
+
+        /// <summary>Produces the no duplicated data.</summary>
+        /// <param name="lotteryNumber">The lottery number.</param>
+        /// <returns>
+        ///   <br />
+        /// </returns>
+        private List<int> produceNoDuplicatedData(int[] lotteryNumber)
+        {
+            var distinctedNumbers = lotteryNumber.Distinct().ToList();  //Use "Distinct" to clear  the duplicated number
+            while (distinctedNumbers.Count != lotteryNumber.Length)
+            {
+                distinctedNumbers.ForEach(x => MessageBox.Show(x.ToString()));  //Appearing the duplicated number, message box will run elements first except the repeat one.
+
+                for (int index = distinctedNumbers.Count; index < lotteryNumber.Length; index++)
+                {
+                    distinctedNumbers.Add(new Random().Next(1, 100));   //Create a new number for lotterynumber
+                }
+
+                distinctedNumbers = distinctedNumbers.Distinct().ToList(); //Check again to ensure every number is single.
+            }
+
+            return distinctedNumbers;
+        } //end of produceNoDuplicatedData
+
+        #endregion function
+    }
+}
