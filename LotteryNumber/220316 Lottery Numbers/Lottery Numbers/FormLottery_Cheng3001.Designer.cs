058eda7d600883ef35ac44f8e7b259945886d645
ye1206 <116014300+ye1206@users.noreply.github.com>
2022-10-20 21:37:14 +0800

    Add files

---
+++ LotteryNumber/220316 Lottery Numbers/Lottery Numbers/FormLottery_Cheng3001.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Lottery_Numbers
+{
+    partial class FormLottery_Cheng3001
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl1st = new System.Windows.Forms.Label();
+            this.Lbl2nd = new System.Windows.Forms.Label();
+            this.Lbl3rd = new System.Windows.Forms.Label();
+            this.Lbl4th = new System.Windows.Forms.Label();
+            this.Lbl5th = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.Lbl10th = new System.Windows.Forms.Label();
+            this.Lbl9th = new System.Windows.Forms.Label();
+            this.Lbl8th = new System.Windows.Forms.Label();
+            this.Lbl7th = new System.Windows.Forms.Label();
+            this.Lbl6th = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Lbl1st
+            // 
+            this.Lbl1st.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl1st.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl1st.Location = new System.Drawing.Point(21, 12);
+            this.Lbl1st.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl1st.Name = "Lbl1st";
+            this.Lbl1st.Size = new System.Drawing.Size(63, 32);
+            this.Lbl1st.TabIndex = 0;
+            this.Lbl1st.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl2nd
+            // 
+            this.Lbl2nd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl2nd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl2nd.Location = new System.Drawing.Point(93, 12);
+            this.Lbl2nd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl2nd.Name = "Lbl2nd";
+            this.Lbl2nd.Size = new System.Drawing.Size(63, 32);
+            this.Lbl2nd.TabIndex = 1;
+            this.Lbl2nd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl3rd
+            // 
+            this.Lbl3rd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl3rd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl3rd.Location = new System.Drawing.Point(165, 12);
+            this.Lbl3rd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl3rd.Name = "Lbl3rd";
+            this.Lbl3rd.Size = new System.Drawing.Size(63, 32);
+            this.Lbl3rd.TabIndex = 2;
+            this.Lbl3rd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl4th
+            // 
+            this.Lbl4th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl4th.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl4th.Location = new System.Drawing.Point(237, 12);
+            this.Lbl4th.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl4th.Name = "Lbl4th";
+            this.Lbl4th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl4th.TabIndex = 3;
+            this.Lbl4th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl5th
+            // 
+            this.Lbl5th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl5th.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl5th.Location = new System.Drawing.Point(309, 12);
+            this.Lbl5th.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl5th.Name = "Lbl5th";
+            this.Lbl5th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl5th.TabIndex = 4;
+            this.Lbl5th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(309, 90);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(112, 47);
+            this.generateButton.TabIndex = 5;
+            this.generateButton.Text = "Generate Numbers";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(430, 90);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 47);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Lbl10th
+            // 
+            this.Lbl10th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl10th.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl10th.Location = new System.Drawing.Point(751, 9);
+            this.Lbl10th.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl10th.Name = "Lbl10th";
+            this.Lbl10th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl10th.TabIndex = 11;
+            this.Lbl10th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl9th
+            // 
+            this.Lbl9th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl9th.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl9th.Location = new System.Drawing.Point(679, 9);
+            this.Lbl9th.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl9th.Name = "Lbl9th";
+            this.Lbl9th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl9th.TabIndex = 10;
+            this.Lbl9th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl8th
+            // 
+            this.Lbl8th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl8th.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl8th.Location = new System.Drawing.Point(607, 9);
+            this.Lbl8th.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl8th.Name = "Lbl8th";
+            this.Lbl8th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl8th.TabIndex = 9;
+            this.Lbl8th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl7th
+            // 
+            this.Lbl7th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl7th.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl7th.Location = new System.Drawing.Point(535, 9);
+            this.Lbl7th.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl7th.Name = "Lbl7th";
+            this.Lbl7th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl7th.TabIndex = 8;
+            this.Lbl7th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbl6th
+            // 
+            this.Lbl6th.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl6th.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl6th.Location = new System.Drawing.Point(463, 9);
+            this.Lbl6th.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl6th.Name = "Lbl6th";
+            this.Lbl6th.Size = new System.Drawing.Size(63, 32);
+            this.Lbl6th.TabIndex = 7;
+            this.Lbl6th.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 163);
+            this.Controls.Add(this.Lbl10th);
+            this.Controls.Add(this.Lbl9th);
+            this.Controls.Add(this.Lbl8th);
+            this.Controls.Add(this.Lbl7th);
+            this.Controls.Add(this.Lbl6th);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.Lbl5th);
+            this.Controls.Add(this.Lbl4th);
+            this.Controls.Add(this.Lbl3rd);
+            this.Controls.Add(this.Lbl2nd);
+            this.Controls.Add(this.Lbl1st);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Lottery Numbers";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl1st;
+        private System.Windows.Forms.Label Lbl2nd;
+        private System.Windows.Forms.Label Lbl3rd;
+        private System.Windows.Forms.Label Lbl4th;
+        private System.Windows.Forms.Label Lbl5th;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label Lbl10th;
+        private System.Windows.Forms.Label Lbl9th;
+        private System.Windows.Forms.Label Lbl8th;
+        private System.Windows.Forms.Label Lbl7th;
+        private System.Windows.Forms.Label Lbl6th;
+    }
+}
+
---
+++ LotteryNumber/220316 Lottery Numbers/Lottery Numbers/FormLottery_Cheng3001.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+//http://loveandcocoa.blogspot.com/2012/06/c-list.html
+
+namespace Lottery_Numbers
+{
+    public partial class FormLottery_Cheng3001 : Form
+    {
+        const int SIZE = 10;
+        int[] lotteryNumber = new int[SIZE];
+        Label[] randomLabel = new Label[SIZE];
+        public FormLottery_Cheng3001()
+        {
+            InitializeComponent();
+            initializeLabelArray();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            for (int index = 0; index < lotteryNumber.Length; index++)
+            {
+                lotteryNumber[index] = rnd.Next(1, 100);
+                randomLabel[index].Text = lotteryNumber[index].ToString();  //將lotterynumber按照索引指定給相同位置的label
+
+                //Before:    Lbl1st.Text = lotteryNumber[0].ToString();     (Others the same as Lbl1st)
+            }
+
+            var distinctedNumbers = produceNoDuplicatedData(lotteryNumber); //When use "var" to define a variance, you don't need to give a data type. It will define its type automatically by the things you give behind.
+
+            for (int index = 0; index < lotteryNumber.Length; index++)
+            {
+                randomLabel[index].Text = distinctedNumbers[index].ToString();  //Same as Line 35
+            }
+        }
+
+        //Intialize array
+        private void initializeLabelArray()
+        {
+            Label[] randomLabel2 = { Lbl1st, Lbl2nd, Lbl3rd, Lbl4th, Lbl5th, Lbl6th, Lbl7th, Lbl8th, Lbl9th, Lbl10th };
+            randomLabel = randomLabel2;
+        }
+
+
+        /// <summary>Produces the no duplicated data.</summary>
+        /// <param name="lotteryNumber">The lottery number.</param>
+        /// <returns>
+        ///   <br />
+        /// </returns>
+        private List<int> produceNoDuplicatedData(int[] lotteryNumber)
+        {
+            var distinctedNumbers = lotteryNumber.Distinct().ToList();  //Use "Distinct" to clear  the duplicated number
+            while (distinctedNumbers.Count != lotteryNumber.Length)
+            {
+                distinctedNumbers.ForEach(x => MessageBox.Show(x.ToString()));  //Appearing the duplicated number, message box will run elements first except the repeat one.
+
+                for (int index = distinctedNumbers.Count; index < lotteryNumber.Length; index++)
+                {
+                    distinctedNumbers.Add(new Random().Next(1, 100));   //Create a new number for lotterynumber
+                }
+                
+                distinctedNumbers = distinctedNumbers.Distinct().ToList(); //Check again to ensure every number is single.
+            }
+
+            return distinctedNumbers;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
