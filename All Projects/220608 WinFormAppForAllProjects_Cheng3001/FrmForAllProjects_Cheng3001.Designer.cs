6d6c0d0ae3644e5a68b3560b6ea1c2dce24b138d
ye1206 <116014300+ye1206@users.noreply.github.com>
2022-10-20 21:46:26 +0800

    Bank and MDI

---
+++ All Projects/220608 WinFormAppForAllProjects_Cheng3001/FrmForAllProjects_Cheng3001.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace WinFormAppForAllProjects_Cheng3001_220608
+{
+    partial class FrmForAllProjects_Cheng3001
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.ColorThemeTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.LotteryNumberTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.BankTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.CreateAccountTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.ReadAccountTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.QueryTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.UnderQueryTSS = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.WindowTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.CascadeTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.HorizontalTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.VerticalTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.LINQTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.GradeTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.WorkerTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.ImageProcessingTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileTSMI,
+            this.WindowTSMI});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileTSMI
+            // 
+            this.FileTSMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ColorThemeTSMI,
+            this.LotteryNumberTSMI,
+            this.LINQTSMI,
+            this.BankTSMI,
+            this.ImageProcessingTSMI,
+            this.UnderQueryTSS,
+            this.ExitTSMI});
+            this.FileTSMI.Name = "FileTSMI";
+            this.FileTSMI.Size = new System.Drawing.Size(55, 28);
+            this.FileTSMI.Text = "File";
+            // 
+            // ColorThemeTSMI
+            // 
+            this.ColorThemeTSMI.Name = "ColorThemeTSMI";
+            this.ColorThemeTSMI.Size = new System.Drawing.Size(270, 34);
+            this.ColorThemeTSMI.Text = "Color Theme";
+            this.ColorThemeTSMI.Click += new System.EventHandler(this.ColorThemeTSMI_Click);
+            // 
+            // LotteryNumberTSMI
+            // 
+            this.LotteryNumberTSMI.Name = "LotteryNumberTSMI";
+            this.LotteryNumberTSMI.Size = new System.Drawing.Size(270, 34);
+            this.LotteryNumberTSMI.Text = "Lottery Number";
+            this.LotteryNumberTSMI.Click += new System.EventHandler(this.LotteryNumberTSMI_Click);
+            // 
+            // BankTSMI
+            // 
+            this.BankTSMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateAccountTSMI,
+            this.ReadAccountTSMI,
+            this.QueryTSMI});
+            this.BankTSMI.Name = "BankTSMI";
+            this.BankTSMI.Size = new System.Drawing.Size(270, 34);
+            this.BankTSMI.Text = "Bank";
+            // 
+            // CreateAccountTSMI
+            // 
+            this.CreateAccountTSMI.Name = "CreateAccountTSMI";
+            this.CreateAccountTSMI.Size = new System.Drawing.Size(241, 34);
+            this.CreateAccountTSMI.Text = "Create Account";
+            this.CreateAccountTSMI.Click += new System.EventHandler(this.CreateAccountTSMI_Click);
+            // 
+            // ReadAccountTSMI
+            // 
+            this.ReadAccountTSMI.Name = "ReadAccountTSMI";
+            this.ReadAccountTSMI.Size = new System.Drawing.Size(241, 34);
+            this.ReadAccountTSMI.Text = "Read Account";
+            this.ReadAccountTSMI.Click += new System.EventHandler(this.ReadAccountTSMI_Click);
+            // 
+            // QueryTSMI
+            // 
+            this.QueryTSMI.Name = "QueryTSMI";
+            this.QueryTSMI.Size = new System.Drawing.Size(241, 34);
+            this.QueryTSMI.Text = "Query Account";
+            this.QueryTSMI.Click += new System.EventHandler(this.QueryTSMI_Click);
+            // 
+            // UnderQueryTSS
+            // 
+            this.UnderQueryTSS.Name = "UnderQueryTSS";
+            this.UnderQueryTSS.Size = new System.Drawing.Size(267, 6);
+            // 
+            // ExitTSMI
+            // 
+            this.ExitTSMI.Name = "ExitTSMI";
+            this.ExitTSMI.Size = new System.Drawing.Size(270, 34);
+            this.ExitTSMI.Text = "Exit";
+            this.ExitTSMI.Click += new System.EventHandler(this.ExitTSMI_Click);
+            // 
+            // WindowTSMI
+            // 
+            this.WindowTSMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CascadeTSMI,
+            this.HorizontalTSMI,
+            this.VerticalTSMI});
+            this.WindowTSMI.Name = "WindowTSMI";
+            this.WindowTSMI.Size = new System.Drawing.Size(96, 28);
+            this.WindowTSMI.Text = "Window";
+            // 
+            // CascadeTSMI
+            // 
+            this.CascadeTSMI.Name = "CascadeTSMI";
+            this.CascadeTSMI.Size = new System.Drawing.Size(235, 34);
+            this.CascadeTSMI.Text = "Cascade";
+            this.CascadeTSMI.Click += new System.EventHandler(this.CascadeTSMI_Click);
+            // 
+            // HorizontalTSMI
+            // 
+            this.HorizontalTSMI.Name = "HorizontalTSMI";
+            this.HorizontalTSMI.Size = new System.Drawing.Size(235, 34);
+            this.HorizontalTSMI.Text = "Tile Horizontal";
+            this.HorizontalTSMI.Click += new System.EventHandler(this.HorizontalTSMI_Click);
+            // 
+            // VerticalTSMI
+            // 
+            this.VerticalTSMI.Name = "VerticalTSMI";
+            this.VerticalTSMI.Size = new System.Drawing.Size(235, 34);
+            this.VerticalTSMI.Text = "Tile Vertical";
+            this.VerticalTSMI.Click += new System.EventHandler(this.VerticalTSMI_Click);
+            // 
+            // LINQTSMI
+            // 
+            this.LINQTSMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GradeTSMI,
+            this.WorkerTSMI});
+            this.LINQTSMI.Name = "LINQTSMI";
+            this.LINQTSMI.Size = new System.Drawing.Size(270, 34);
+            this.LINQTSMI.Text = "LINQ";
+            // 
+            // GradeTSMI
+            // 
+            this.GradeTSMI.Name = "GradeTSMI";
+            this.GradeTSMI.Size = new System.Drawing.Size(270, 34);
+            this.GradeTSMI.Text = "Grade";
+            this.GradeTSMI.Click += new System.EventHandler(this.GradeTSMI_Click);
+            // 
+            // WorkerTSMI
+            // 
+            this.WorkerTSMI.Name = "WorkerTSMI";
+            this.WorkerTSMI.Size = new System.Drawing.Size(270, 34);
+            this.WorkerTSMI.Text = "Worker";
+            this.WorkerTSMI.Click += new System.EventHandler(this.WorkerTSMI_Click);
+            // 
+            // ImageProcessingTSMI
+            // 
+            this.ImageProcessingTSMI.Name = "ImageProcessingTSMI";
+            this.ImageProcessingTSMI.Size = new System.Drawing.Size(270, 34);
+            this.ImageProcessingTSMI.Text = "Image Processing";
+            this.ImageProcessingTSMI.Click += new System.EventHandler(this.ImageProcessingTSMI_Click);
+            // 
+            // FrmForAllProjects_Cheng3001
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmForAllProjects_Cheng3001";
+            this.Text = " ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileTSMI;
+        private System.Windows.Forms.ToolStripMenuItem ExitTSMI;
+        private System.Windows.Forms.ToolStripMenuItem WindowTSMI;
+        private System.Windows.Forms.ToolStripMenuItem CascadeTSMI;
+        private System.Windows.Forms.ToolStripMenuItem HorizontalTSMI;
+        private System.Windows.Forms.ToolStripMenuItem VerticalTSMI;
+        private System.Windows.Forms.ToolStripMenuItem ColorThemeTSMI;
+        private System.Windows.Forms.ToolStripMenuItem LotteryNumberTSMI;
+        private System.Windows.Forms.ToolStripMenuItem BankTSMI;
+        private System.Windows.Forms.ToolStripMenuItem CreateAccountTSMI;
+        private System.Windows.Forms.ToolStripMenuItem ReadAccountTSMI;
+        private System.Windows.Forms.ToolStripMenuItem QueryTSMI;
+        private System.Windows.Forms.ToolStripSeparator UnderQueryTSS;
+        private System.Windows.Forms.ToolStripMenuItem LINQTSMI;
+        private System.Windows.Forms.ToolStripMenuItem GradeTSMI;
+        private System.Windows.Forms.ToolStripMenuItem WorkerTSMI;
+        private System.Windows.Forms.ToolStripMenuItem ImageProcessingTSMI;
+    }
+}
---
+++ All Projects/220608 WinFormAppForAllProjects_Cheng3001/FrmForAllProjects_Cheng3001.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CreateFile_TextAndByte_Cheng3001;
+using ReadSequentialAccessFile_TextAndByte_Cheng3001;
+using CreditInquirty_TextAndByte_Cheng3001;
+using SimpleColorMixture_Cheng3001;
+using LotteryNumber_Cheng3001;
+using WinFormsApp1UsingLINQ_Cheng3001_220413;
+using ;
+using WinFormsApp4Bank_Cheng3001_220504;
+using ClassLibrary_Cheng3001.FileStreams_Cheng3001;
+
+namespace WinFormAppForAllProjects_Cheng3001_220608
+{
+    public partial class FrmForAllProjects_Cheng3001 : Form
+    {
+        public FrmForAllProjects_Cheng3001()
+        {
+            InitializeComponent();
+        }
+
+        #region File
+
+        private void ColorThemeTSMI_Click(object sender, EventArgs e)
+        {
+            var child = new FrmSimpleColorMixture_Cheng3001(); // create new child
+            child.MdiParent = this; // set parent
+            child.Show(); // display child    
+        } //end of ColorThemeTSMI_Click
+
+        private void LotteryNumberTSMI_Click(object sender, EventArgs e)
+        {
+            var child = new FrmLotteryNumber_Cheng3001(); // create new child
+            child.MdiParent = this; // set parent
+            child.Show(); // display child    
+        } //end of LotteryNumberTSMI_Click
+
+        #region LINQ
+        private void GradeTSMI_Click(object sender, EventArgs e)
+        {
+            var child = new FrmAppForListAndArray_Cheng3001(); // create new child
+            child.MdiParent = this; // set parent
+            child.Show(); // display child    
+        } //end of GradeTSMI_Click
+        
+        private void WorkerTSMI_Click(object sender, EventArgs e)
+        {
+            var child = new FrmForWorker_Cheng3001_220525(); // create new child
+            child.MdiParent = this; // set parent
+            child.Show(); // display child    
+        } //end of WorkerTSMI_Click
+        #endregion LINQ
+
+        #region Bank
+        private void CreateAccountTSMI_Click(object sender, EventArgs e)
+        {
+            var child = new FrmCreateFile_TextAndByte_Cheng3001(); // create new child
+            child.MdiParent = this; // set parent
+            child.Show(); // display child    
+        } //end of CreateAccountsTSMI_Click
+
+        private void ReadAccountTSMI_Click(object sender, EventArgs e)
+        {
+            var child = new FrmReadFile_TextNByte_Cheng3001();
+            child.MdiParent = this;
+            child.Show();
+        } //end of ReadAccountTSMI_Click
+
+        private void QueryTSMI_Click(object sender, EventArgs e)
+        {
+            var child = new FrmCreditInquirty_TextAndByte_Cheng3001();
+            child.MdiParent = this;
+            child.Show();
+        } //end of QueryTSMI_Click
+        #endregion Bank
+
+        private void ImageProcessingTSMI_Click(object sender, EventArgs e)
+        {
+
+        } //end of ImageProcessingTSMI_Click
+
+        private void ExitTSMI_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        } //end of ExitTSMI_Click
+
+        #endregion File
+
+        #region Windows
+        private void CascadeTSMI_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        } //end of CascadeTSMI_Click
+
+        private void HorizontalTSMI_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        } //end of HorizontalTSMI_Click
+
+        private void VerticalTSMI_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        } //end of VerticalTSMI_Click
+        #endregion Windows
+
+
+
+
+
+        
+    }
+}
